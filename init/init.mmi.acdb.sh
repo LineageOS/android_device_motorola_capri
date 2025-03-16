c912ae56f9434704c5ddd30049867dd8680adddd
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-16 02:16:44 -0300

    capri: init: Update acdb script for power variant
    This is neeeded because the acdb files in etc/acdbdata/common/ path
    were moved to etc/acdbdata/ because persist.vendor.audio.calfile props
    are set in sm6225-common, so change it to make the 6000 mAh variant
    work properly.
    
    Change-Id: I82a3018855810398bec69147bdd3cf9fcc96d2ee

--- init/init.mmi.acdb.sh
+++ init/init.mmi.acdb.sh
@@ -7,16 +7,15 @@ notice()
 }
 
 path=/vendor/etc/acdbdata
-device=common
 
 # For capri power, use seprate parameter files.
 if [ "`getprop ro.vendor.hw.batt`" == "6000" ]; then
-	device=power
+	path=/vendor/etc/acdbdata/power
 fi
 
 index=0
-for file in $(ls $path/$device/); do
-    setprop persist.vendor.audio.calfile$index $path/$device/$file
+for file in $(ls $path/); do
+    setprop persist.vendor.audio.calfile$index $path/$file
     index=$((index+1))
 done
 notice "hw.batt: [`getprop ro.vendor.hw.batt`], calfile6: [`getprop persist.vendor.audio.calfile6`]"
