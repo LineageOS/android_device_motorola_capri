cee0f1346d2a85d6050cbf6af0865a61ad4bb210
Deivid21 <david.parra.ignacio@gmail.com>
2025-10-15 01:24:28 -0300

    capri: Patch vidhance components with libui-v34
    * EIS was broken, which caused hard crashes when switching
      to video mode in camera.
    
    Change-Id: If7d3b4417208fbb51466992aece7ec9b82a1cd41

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,11 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
     'vendor/lib64/liblowlight_ai_portrait.arcsoft.so': blob_fixup()
         .fix_soname(),
+    (
+        'vendor/lib64/camera/components/com.qti.node.dewarp.so',
+        'vendor/lib64/camera/components/com.vidhance.node.processing.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
 }  # fmt: skip
 
 extract_fns: extract_fns_user_type = {
